521f3182b35d3dbcd2dcfa747fd7dd7c1269fdb6
carsin <20868514+carsin@users.noreply.github.com>
2020-05-30 18:54:11 -1000

    Remove unneeded u8 types

--- age_100_calculator/src/main.rs
+++ age_100_calculator/src/main.rs
@@ -7,9 +7,9 @@ use chrono::prelude::*;
 fn main() {
     let name = input(("What is your name? ").to_string());
     let age = input(format!("How old are you, {}? ", &name))
-        .parse::<u8>().expect("Failed converting age to number");
+        .parse().expect("Failed converting age to number");
 
-    let centennial_year = calculate_year_of_centennial(age as u8);
+    let centennial_year = calculate_year_of_centennial(age);
     println!("{}, you will turn 100 during the year {}!", &name, &centennial_year);
 }
 
@@ -28,11 +28,11 @@ fn input(user_message: String) -> String {
     input
 }
 
-fn calculate_year_of_centennial(age: u8) -> i32 {
+fn calculate_year_of_centennial(age: i32) -> i32 {
     // Get current year
     let current_year = Utc::today().year();
     // Calculate years left until age 100
-    let years: i32 = 99 - age as i32;
+    let years = 100 - age;
 
     &current_year + &years
 }
