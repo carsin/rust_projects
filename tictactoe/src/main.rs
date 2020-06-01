e4595f681d4e5cebf3c1601f847302d8f371a910
carsin <20868514+carsin@users.noreply.github.com>
2020-05-31 18:38:40 -1000

    WHY DOESNT THIS WORK

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -14,35 +14,48 @@ fn main() {
     }
 }
 
-fn print_board(board: &[[i64; 3]; 3]) {
+fn print_board(board: &[[u8; 3]; 3]) {
+    print!("┌───┬───┬───┐\n");
     for x in 0..3 {
+        print!("│ ");
         for y in 0..3 {
             match board[x][y] {
-                0 => print!("_"),
+                0 => print!("."),
                 1 => print!("X"),
                 2 => print!("O"),
                 _ => print!("Error!"),
             }
-            io::stdout().flush().unwrap();
+            print!(" │ ");
         }
-        println!("");
+        print!("\n");
     }
+    print!("└───┴───┴───┘\n");
+
+    io::stdout().flush().unwrap();
 }
 
-fn get_player_input(turn: &bool) {
+fn get_player_input(turn: &bool) -> u8 {
     let mut x_input = String::new();
     while x_input == String::new() {
         print!("Turn: {}, Choose x position: ", &turn);
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut x_input).expect("Failed");
 
-        if x_input.trim().parse::<i64>().is_ok() {
-            x_input = x_input.trim().parse().unwrap();
-            println!("x: {}", x_input);
-            continue;
+        if x_input.trim().parse::<u8>().is_ok() {
+            x_input = x_input.trim().parse::<u8>().unwrap();
+            if x_input > 3 { // Why do I have to convert 3 to a string?
+                println!("Must input a number lower than 3");
+                let x_input = String::new();
+            } else {
+                println!("x: {}", x_input);
+            }
         } else {
             println!("Not a number: {}", x_input);
             x_input = String::new();
         }
     }
+
+    let y_input: u8 = 0;
+
+    x_input
 }
