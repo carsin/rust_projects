0e6a777232c5ade34ad5e8207062f5004eb49b2c
carsin <20868514+carsin@users.noreply.github.com>
2020-05-31 15:58:37 -1000

    Finish find nth word v1

--- find_nth_word/src/main.rs
+++ find_nth_word/src/main.rs
@@ -1,22 +1,12 @@
 fn main() {
     let string = String::from("carson is my name");
-    println!("{}", find_first_word(&string));
-    println!("{}", find_second_word(&string));
+    let first_word = find_nth_word(&string, 1);
+    let second_word = find_nth_word(&string, 2);
+    println!("{}", first_word);
+    println!("{}", second_word);
 }
 
-fn find_first_word(s: &String) -> &str {
-    let bytes = s.as_bytes(); // convert string to an array of bytes
-
-    for (i, &item) in bytes.iter().enumerate() { // i is the index in the tuple and &item is the byte in the tuple
-        if item == b' ' { // check if the character is a space
-            return &s[0..i]; // return the slice of the beginning of the string to the first space
-        }
-    }
-
-    &s[..]
-}
-
-fn find_second_word(input_string: &String) -> &str {
+fn find_nth_word(input_string: &str, n: i32) -> &str {
     let bytes = input_string.as_bytes();
     let mut space_count = 0;
     let mut first_space_index = 0;
@@ -25,10 +15,9 @@ fn find_second_word(input_string: &String) -> &str {
         if current_char == b' ' {
             space_count += 1;
 
-            if space_count == 1 {
-                first_space_index = i;
-            } else if space_count == 2 {
-                // let mut second_space_index = i;
+            if space_count == n - 1 {
+                first_space_index = i + 1;
+            } else if space_count == n {
                 return &input_string[first_space_index..i];
             }
         }
@@ -36,4 +25,3 @@ fn find_second_word(input_string: &String) -> &str {
 
     &input_string
 }
-
