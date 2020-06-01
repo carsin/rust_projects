48c7e71ac87c67b6aea2504b10559e1598b8eea4
carsin <20868514+carsin@users.noreply.github.com>
2020-05-31 17:43:25 -1000

    ttt: Beginnings of getting player input

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -2,12 +2,19 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    let mut board = [[0u8; 3]; 3];
+    let board = [[0; 3]; 3];
+    let mut turn = false;
 
-    print_board(&board);
+    let playing = true;
+    while playing {
+        print_board(&board);
+        get_player_input(&turn);
+
+        turn = !turn;
+    }
 }
 
-fn print_board(board: &[[u8; 3]; 3]) {
+fn print_board(board: &[[i64; 3]; 3]) {
     for x in 0..3 {
         for y in 0..3 {
             match board[x][y] {
@@ -19,5 +26,23 @@ fn print_board(board: &[[u8; 3]; 3]) {
             io::stdout().flush().unwrap();
         }
         println!("");
+    }
+}
+
+fn get_player_input(turn: &bool) {
+    let mut x_input = String::new();
+    while x_input == String::new() {
+        print!("Turn: {}, Choose x position: ", &turn);
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut x_input).expect("Failed");
+
+        if x_input.trim().parse::<i64>().is_ok() {
+            x_input = x_input.trim().parse().unwrap();
+            println!("x: {}", x_input);
+            continue;
+        } else {
+            println!("Not a number: {}", x_input);
+            x_input = String::new();
+        }
     }
 }
