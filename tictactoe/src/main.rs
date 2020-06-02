fd1410a7d37fb8843f84c845ca6d7a58c1f2f2c6
carsin <20868514+carsin@users.noreply.github.com>
2020-06-02 13:02:03 -1000

    restart game after win

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -23,20 +23,62 @@ fn main() {
         println!("\n{}'s turn.", turn_as_char);
         let last_play_coords = get_player_input();
 
+        // Check if position is empty
         if board[last_play_coords.y][last_play_coords.x] == 0 {
             board[last_play_coords.y][last_play_coords.x] = turn_as_int;
             if check_for_win(&board, &last_play_coords, turn_as_int) {
-                println!("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 print_board(&board);
                 println!("{} wins!", turn_as_char);
-                io::stdin().read_line(&mut String::new()).unwrap();
-                break;
+                board = [[0u8; 3]; 3];
             }
-            turn = !turn;
+            // turn = !turn;
         } else {
             println!("That space is already occupied!");
         }
     }
+}
+
+fn get_player_input() -> Coordinate {
+    let mut x_input = String::new();
+    let mut x: usize = 0;
+
+    while x_input == String::new() {
+        print!("X: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut x_input).expect("Failed");
+
+        if x_input.trim().parse::<usize>().is_ok() {
+            x = x_input.trim().parse::<usize>().unwrap();
+            if x > 3 {
+                println!("Must input a number lower than 3");
+                x_input = String::new();
+            }
+        } else {
+            println!("Not a number: {}", x_input.trim());
+            x_input = String::new();
+        }
+    }
+
+    let mut y_input: String = String::new();
+    let mut y: usize = 0;
+    while y_input == String::new() {
+        print!("Y: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut y_input).expect("Failed");
+
+        if y_input.trim().parse::<usize>().is_ok() {
+            y = y_input.trim().parse::<usize>().unwrap();
+            if y > 3 {
+                println!("Must input a number lower than 3");
+                y_input = String::new();
+            }
+        } else {
+            println!("Not a number: {}", y_input.trim());
+            y_input = String::new();
+        }
+    }
+
+    Coordinate { y: y - 1, x: x - 1 }
 }
 
 fn check_for_win(board: &[[u8; 3]; 3], last_move: &Coordinate, turn_as_int: u8) -> bool {
@@ -95,47 +137,4 @@ fn print_board(board: &[[u8; 3]; 3]) {
     print!("  └───┴───┴───┘\n");
 
     io::stdout().flush().unwrap();
-}
-
-fn get_player_input() -> Coordinate {
-    let mut x_input = String::new();
-    let mut x: usize = 0;
-
-    while x_input == String::new() {
-        print!("X: ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut x_input).expect("Failed");
-
-        if x_input.trim().parse::<usize>().is_ok() {
-            x = x_input.trim().parse::<usize>().unwrap();
-            if x > 3 {
-                println!("Must input a number lower than 3");
-                x_input = String::new();
-            }
-        } else {
-            println!("Not a number: {}", x_input.trim());
-            x_input = String::new();
-        }
-    }
-
-    let mut y_input = String::new();
-    let mut y: usize = 0;
-    while y_input == String::new() {
-        print!("Y: ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut y_input).expect("Failed");
-
-        if y_input.trim().parse::<usize>().is_ok() {
-            y = y_input.trim().parse::<usize>().unwrap();
-            if y > 3 {
-                println!("Must input a number lower than 3");
-                y_input = String::new();
-            }
-        } else {
-            println!("Not a number: {}", y_input.trim());
-            y_input = String::new();
-        }
-    }
-
-    Coordinate { y: y - 1, x: x - 1 }
 }
