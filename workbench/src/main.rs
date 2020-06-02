55a748630beea06bf1313a83ce8d3ac3471419d1
carsin <20868514+carsin@users.noreply.github.com>
2020-06-01 16:58:17 -1000

    Workbench: testing structs

--- workbench/src/main.rs
+++ workbench/src/main.rs
@@ -1,13 +1,21 @@
-use rand::Rng;
-
-fn main() {
-    let num = gen_number();
-
-    println!("{}", num);
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn gen_number() -> u32 {
-    let x = rand::thread_rng().gen_range(0, 5);
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
 
-    x
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {}", rect1.area());
+    println!("rect1 is {:?}", rect1);
 }
